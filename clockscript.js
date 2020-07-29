4f2255eb5446f69a4213d744c1edb22039be14e2
mwilliams659 <63402316+mwilliams659@users.noreply.github.com>
2020-07-29 12:44:53 +0100

    Adding live clock function

---
+++ clockscript.js
@@ -0,0 +1,32 @@
+// var today = new Date();
+
+// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+
+//   new Date().toLocaleTimeString();
+
+
+
+function getDateTime() {
+        var now     = new Date(); 
+        var hour    = now.getHours();
+        var minute  = now.getMinutes();
+        // var second  = now.getSeconds();  
+        if(hour.toString().length == 1) {
+             hour = '0'+hour;
+        }
+        if(minute.toString().length == 1) {
+             minute = '0'+minute;
+        }
+        // if(second.toString().length == 1) {
+        //      second = '0'+second;
+        // }   
+        var dateTime = hour+':'+minute;  
+         return dateTime;
+    }
+
+// real time
+    setInterval(function(){
+        currentTime = getDateTime();
+        document.getElementById("digital-clock").innerHTML = currentTime;
+    }, 1000);
