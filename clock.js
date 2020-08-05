a3af3c10342eaec4c5f4ddc9bbb66cd818aa31c4
mwilliams659 <63402316+mwilliams659@users.noreply.github.com>
2020-08-05 16:24:22 +0100

    Added interval

--- clock.js
+++ clock.js
@@ -24,15 +24,17 @@ document.getElementById("digital-clock").innerHTML = currentTime;
 }, 100);
 
 
-var timesList = ['00:00', '01:10', '01:23', '08:50', '11:11', '12:21', '12:34', '13:57', '16:20', '20:20', '21:21', '22:22', '23:23', '23:45'];
+var timesList = ['00:00', '01:10', '01:23', '08:50', '11:11', '12:21', '12:34', '13:57', '16:20', '16:25', '20:20', '21:21', '22:22', '23:23', '23:45'];
 
 
+setInterval(function(){
+    if (timesList.includes(currentTime)) {
+        document.getElementById("background").className = "wowTime";
+        }
+    else {
+        document.getElementById("regularTime")
+    }
+}, 100);
 
-if (timesList.includes(currentTime)) {
-document.getElementById("background").className = "wowTime";
-}
-else {
-    document.getElementById("regularTime")
-};
 
 
