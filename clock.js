3cf4910ffa97840377bf1f85bf04e99d143c591a
mwilliams659 <63402316+mwilliams659@users.noreply.github.com>
2021-04-28 17:41:10 +0100

    User to input time which is saved to local storage.

--- clock.js
+++ clock.js
@@ -14,6 +14,7 @@ function getTime() {
     return dateTime;
 }
 
+
 // displays the current time 
 var currentTime = getTime()
 setInterval(function(){
@@ -34,41 +35,50 @@ play.onclick = function() {
 var timesList = ['00:00', '01:01', '01:10', '01:11', '01:23', '02:02', '02:20', '02:22', '03:00', '03:03', '03:33', '04:04', '04:20', '05:05', '05:55', '06:00', '06:06', '07:07', '08:08', '08:50', '09:00', '09:09', '10:10', '11:11', '12:00', '12:12', '12:21', '12:34', '13:13', '13:57', '14:14', '15:00', '15:15', '16:16', '16:20', '17:17', '18:00', '18:18', '19:19', '20:20', '21:00', '21:21', '22:22', '23:23', '23:45'];
 var snoopList = ['04:20', '16:20'];
 
-// Checks time to change elements
-setInterval(function(){
-    if (timesList.includes(currentTime)) {
-        document.getElementById("animate-this").className = "container funAnime animated";
-        var now     = new Date(); 
-        var second  = now.getSeconds();
-        if (second === 0) {
-            // this is when the audio gets played
-            if (snoopList.includes(currentTime)) {
-                document.getElementById("audio2").play();
-                document.getElementById("inTheBack").className = "snoopTime";
-                // this is when snoop dogg comes on (i.e it's 04:20/16:20)
-                document.getElementById("snoopDogg").style.display = "block";
-                document.getElementById("smokeDogg").style.display = "block";
-            // This is when the audio, gifs and background get triggered
-            } else {
+// // Checks time to change elements
+setInterval(function() {
+    var now     = new Date(); 
+    var second  = now.getSeconds();
+    if (second == 0) {
+        if (timesList.includes(currentTime)) {
+            document.getElementById("animate-this").className = "container funAnime animated";
             document.getElementById("audio1").play();
             document.getElementById("inTheBack").className = "wowTime";
             document.getElementById("missionPassed").style.display = "block";
             document.getElementById("catDogg").style.display = "block";
-            }
+        } else if (snoopList.includes(currentTime)) {
+            document.getElementById("audio2").play();
+            document.getElementById("inTheBack").className = "snoopTime";
+            document.getElementById("snoopDogg").style.display = "block";
+            document.getElementById("smokeDogg").style.display = "block";
+        } else {
+            document.getElementById("inTheBack").className = "regularTime";
+            document.getElementById("animate-this").className = "container noneAnime animated";
+            document.getElementById("snoopDogg").style.display = "none";
+            document.getElementById("smokeDogg").style.display = "none";
+            document.getElementById("catDogg").style.display = "none";
+            document.getElementById("missionPassed").style.display = "none";
         }
     }
-    else {
-        // this is when there is not a significant time
-        document.getElementById("inTheBack").className = "regularTime";
-        document.getElementById("animate-this").className = "container noneAnime animated";
-        document.getElementById("snoopDogg").style.display = "none";
-        document.getElementById("smokeDogg").style.display = "none";
-        document.getElementById("catDogg").style.display = "none";
-        document.getElementById("missionPassed").style.display = "none";
-
-        
-    }
 }, 100);
+
+
+const addTime = (ev)=> {
+    ev.preventDefault(); 
+    let selected = document.querySelector('input[type="time"]');
+    let selectedTime = selected.value;
+    timesList.push(selectedTime);
+    document.getElementById('timeSelector').value = null;
+
+    console.warn('added', {timesList} );
+
+    // saving to local storage
+    localStorage.setItem('MyTimesList', JSON.stringify(timesList) );
+
+}
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById('btn').addEventListener('click', addTime);
+});
 
 
 
@@ -103,7 +113,6 @@ function setGradient() {
 	+ color2.value 
 	+ ")";
 
-	css.textContent = body.style.background + ";";
 }
 
 // Inputs to select two colours to form background gradient
